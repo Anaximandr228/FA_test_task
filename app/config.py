542292cd7bd8555e0f8d6b4c3ded8cffdd9f0e94
Alex <alex09.g29@gmail.com>
2025-07-25 12:10:28 +0500

    Rename variables

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-host = os.environ.get('POSTGRES_HOST', 'localhost')  # Хост приложения
-user = "postgres"  # Имя пользователя PostgreSQL
-password = "rtf558Ur"  # Пароль пользователя PostgreSQL
-db_name = "fa_db"  # Название базы данных PostgeSQL
+HOST = os.environ.get('POSTGRES_HOST', 'localhost')  # Хост приложения
+USER = "postgres"  # Имя пользователя PostgreSQL
+PASSWORD = "rtf558Ur"  # Пароль пользователя PostgreSQL
+DB_NAME = "fa_db"  # Название базы данных PostgeSQL
